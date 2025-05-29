ef15e3f374dcbcd3115935ef48884de0e3de4dca
Ak154 <anandkumar15446@gmail.com>
2025-05-29 07:51:35 +0530

    Controller file has been added for knowledge routes

--- server/routes/knowledge.js
+++ server/routes/knowledge.js
@@ -1,35 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Knowledge = require('../models/Knowledge');
+const { KnowledgeReq, konwledgeData } = require("../controllers/knowledge")
 
 // Add to knowledge base
-router.post('/', async (req, res) => {
-  try {
-    const { question, answer, tags } = req.body;
-    
-    const newKnowledge = new Knowledge({
-      question,
-      answer,
-      tags: tags ? tags.split(',').map(tag => tag.trim()) : []
-    });
-    
-    await newKnowledge.save();
-    res.json(newKnowledge);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
-});
+router.post('/', KnowledgeReq);
 
 // Get all knowledge items
-router.get('/', async (req, res) => {
-  try {
-    const knowledge = await Knowledge.find().sort({ createdAt: -1 });
-    res.json(knowledge);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
-});
+router.get('/', konwledgeData);
+
+router.post("/", )
 
 module.exports = router;
