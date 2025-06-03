1c7b8356cd7c118459ebcb5c90d835d9cfdfc3b0
Ak154 <anandkumar15446@gmail.com>
2025-06-03 10:18:24 +0530

    User contrroler updated

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,6 @@
 const User = require("../models/Users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.userRegister = async (req, res) => {
   try {
@@ -9,6 +11,11 @@ exports.userRegister = async (req, res) => {
         .status(409)
         .send({ message: "User already exist", success: false });
     }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
 
     let newUser = new User(req.body);
 
@@ -24,4 +31,32 @@ exports.userRegister = async (req, res) => {
   }
 };
 
-exports.userLogin = async (req, res) => {};
+exports.userLogin = async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: "User not found", success: false });
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).send({ message: "Incorrect password" });
+    } else {
+      const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+        expiresIn: "1d",
+      });
+
+      res
+        .status(200)
+        .send({ message: "Log in successfully", success: true, token });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Internal server error", success: false, error });
+  }
+};
